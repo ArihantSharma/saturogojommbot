8392a1d4005e99dad5a725f804a2ef134fb7db47
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2023-03-18 19:46:44 +0530

    Create starter.py

---
+++ Plugins/starter.py
@@ -0,0 +1,25 @@
+from telethon import events
+
+from config import bot
+from Helper.helper import help_text, start_text
+
+
+class start:
+    @bot.on(events.NewMessage(pattern="/start"))
+    async def event_handler_start(event):
+        await bot.send_message(
+            event.chat_id,
+            start_text,
+            file="https://telegra.ph/file/92cf02b20ff395bd5e9e0.jpg",
+        )
+
+    @bot.on(events.NewMessage(pattern="/help"))
+    async def event_handler_help(event):
+        await bot.send_message(event.chat_id, help_text)
+
+    @bot.on(events.NewMessage(pattern="/source"))
+    async def event_handler_source(event):
+        await bot.send_message(
+            event.chat_id,
+            "[Channel](https://t.me/MangaMix)\nThis bot was created by MangaMix",
+        )
