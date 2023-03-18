b7e71145d22d6b0e80a1af1369f875d939ae1b18
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2023-03-18 19:47:47 +0530

    Create helper.py

---
+++ Helper/helper.py
@@ -0,0 +1,16 @@
+start_text = """Welcome to Saturo Gojo Bot. Here you can Download all Mangas for Free.
+
+Please read the Instructions carefully before surfing in...
+
+Use /help to know commands and how to use this bot"""
+
+help_text = """List of commands:
+
+/manga <name of Manga you want> : to download any Manga by search
+
+/read : <name of Manga you want to read>
+/nh : </nh <hentai code>
+
+The files provided are in multiple qualities to download just open file in chrome
+
+To report any Problems, Bugs, Suggestions go to @MangaMix chat group"""
